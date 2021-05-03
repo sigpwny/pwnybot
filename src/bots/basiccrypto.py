602d313c5e0b13041dbc64f95686103c5b2450ee
Peter Stenger <peter.a.stenger@gmail.com>
2021-05-03 16:15:51 -0400

    init breakout rooms

--- src/bots/basiccrypto.py
+++ src/bots/basiccrypto.py
@@ -1,26 +1,21 @@
 # Imports go here
 import discord
 from discord.ext import commands
+from base64 import b32encode, b64encode
 
 # REQUIRED INFORMATION for the bot to work!
 NAME = "basiccrypto"
 VERSION = "1.0.0"
 
-def rawhex(val):
-    try:
-        ret = "`" + str(hex(int(val,10)))[2:]  + "`"
-    except Exception as e:
-        ret = e
-    return ret
 
 class BasicCrypto(commands.Cog):
-    def __init__(self,client):
+    def __init__(self, client):
         self.client = client
 
     @commands.Cog.listener()
     async def on_ready(self):
         print("[pwnyBot] " + NAME + " is online")
-    
+
     @commands.Cog.listener()
     async def on_message(self, message):
         if message.author == self.client.user:
@@ -30,13 +25,20 @@ class BasicCrypto(commands.Cog):
         args = content.split(" ")[1:]
         if command[0] != '!':
             return
-        
-        if command == '!hex':
-            response = rawhex(args[0])
-        elif command == '!b64':
-            response = "WILL ADD b64 tmrw"
-        
+
+        try:
+            if command == '!hex':
+                response = str(hex(int(args[0])))[2:]
+            elif command == '!b64':
+                response = b64encode(args[0])
+            elif command == '!b32':
+                response = b32encode(args[0])
+            response = f'`{response}`'
+        except Exception as e:
+            print(f'WARNING: {e} on message: `{message}`')
+            response = e
         await message.channel.send(response)
+
 
 def setup(client):
     client.add_cog(BasicCrypto(client))
