ca76d1a8fe1fcc94ed665815cc96952612c07778
Faisal Khalid <faisalkhalid@Faisals-MacBook-Pro.local>
2021-05-18 22:06:41 -0500

    Added comments to main.py

--- src/main.py
+++ src/main.py
@@ -11,19 +11,23 @@ from bots import demo
 import discord
 from discord.ext import commands
 
-# OUT_PATH = "./files/"
+# Load .env file (should be in src/ folder)
 load_dotenv()
 
+# Openn and setup our config file
 f = open('config.json', 'r')
 config = json.load(f)
 
+# Load discord client
 client = discord.Client()
 client = commands.Bot(command_prefix = config['PREFIX']) 
 
-# loaded = []
-
 @client.event
 async def on_ready():
+	'''
+	This happens upon start up of the bot. It prints out a little pwnybot message
+	and then prints out the bot version.
+	'''
 	print("################################")
 	print("#                              #")
 	print("#           pwnyBot            #")
@@ -36,64 +40,23 @@ async def on_ready():
 
 @client.command()
 async def load(ctx, extension):
+	'''
+	This function loads in a specified cog. 
+	'''
 	client.load_extension(f'cogs.{extension}')
 
 @client.command()
 async def unload(ctx, extension):
+	'''
+	This function unloads in a specified cog. 
+	'''
 	client.unload_extension(f'cogs.{extension}')
 
+# Goes through our folder and just loads in all of the cogs
 for filename in os.listdir('./bots'):
 	if filename.endswith('.py'):
 		client.load_extension(f'bots.{filename[:-3]}')
 		print("[pwnyBot] Loaded", filename)
 
-print(os.getenv('DISCORD_TOKEN'))
-print(config['PREFIX'])
-
+# Login via token
 client.run(os.getenv('DISCORD_TOKEN'))
-
-# def main():
-# 	os.chdir("src")
-# 	print_startup()
-# 	print_version()
-	
-# 	TOKEN = load_token("token.key")
-# 	client = commands.Bot(command_prefix = '$') 
-
-# 	@client.command()
-# 	async def bot(ctx, cmd, name):
-# 		cmd = cmd.lower()
-# 		if cmd in {'enable','e'}:
-# 			try:
-# 				client.load_extension(f'bots.{name}')
-# 				await ctx.send(f'Bot `{name}` enabled successfully')
-# 			except Exception as e:
-# 				await ctx.send(f'Bot `{name}` failed to load (error message follows)\n```{e}```')
-# 		elif cmd in {'disable','d'}:
-# 			try:
-# 				client.unload_extension(f'bots.{name}')
-# 				await ctx.send(f'Bot `{name}` disabled successfully')
-# 			except Exception as e:
-# 				await ctx.send(f'Bot `{name}` could not be disabled (error message follows)\n```{e}```')
-# 		elif cmd in {'reload','r'}:
-# 			try:
-# 				client.unload_extension(f'bots.{name}')
-# 				client.load_extension(f'bots.{name}')
-# 				await ctx.send(f'Bot `{name}` reloaded successfully')
-# 			except Exception as e:
-# 				await ctx.send(f'Bot `{name}` could not be reloaded (error message follows)\n```{e}```')
-				
-# 	# This is technically cogs but we are calling them bots
-# 	# Stolen from https://www.youtube.com/watch?v=vQw8cFfZPx0 at 6:00
-# 	loaded = load_conf()
-# 	for fname in os.listdir('./bots'):
-# 		if fname.endswith('.py') and fname.split('.py')[0] in loaded:
-# 			client.load_extension(f'bots.{fname[:-3]}')
-# 			print("[pwnyBot] Loaded",fname)
-
-# 	client.run(TOKEN)
-
-
-
-# if __name__ == '__main__':
-# 	main()
