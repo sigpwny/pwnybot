63187a878bd4b69f2633112a7cf17caacbff96b6
Peter Stenger <peter.a.stenger@gmail.com>
2021-08-09 10:50:04 -0400

    Update portal command

--- cogs/portal/portal.py
+++ cogs/portal/portal.py
@@ -8,6 +8,7 @@ from lib.util import command_decorator, subcommand_decorator
 EMOTE_FROM = "<:pwnyPortalFrom:846831813720932383> "
 EMOTE_TO = '<:pwnyPortalTo:846831813136613376> '
 
+
 def gen_link_msg(msg):
     return (
         "https://discord.com/channels/"
@@ -17,17 +18,6 @@ def gen_link_msg(msg):
         + "/"
         + str(msg.id)
     )
-
-def gen_link_ctx(ctx):
-    # import pdb; pdb.set_trace()
-    return (
-        "https://discord.com/channels/"
-        + str(ctx.author.guild.id)
-        + "/"
-        + str(ctx.channel.id)
-        + "/"
-        + str(ctx.interaction_id) # this is incorrect
-        )
 
 
 class Portal(commands.Cog):
@@ -40,6 +30,8 @@ class Portal(commands.Cog):
     @command_decorator({'location': {'description': "The channel to portal to"}})
     async def portal(self, ctx: SlashContext, location: OptionType.CHANNEL) -> None:
         """Create a portal to a channel"""
+        await ctx.defer()
+
         dst_msg = await location.send(
             "filler"
         )
@@ -47,12 +39,12 @@ class Portal(commands.Cog):
             "filler"
         )
         await dst_msg.edit(
-            # format for channel linking: <#874206111719374858>
-            # NOTE: it seems like editing requires that the emote is in the server in question.
-            content = EMOTE_TO + " <#" + str(src_msg.channel.id) + "> " + gen_link_msg(src_msg)
+            content=EMOTE_TO +
+            f" <#{src_msg.channel.id}> " + gen_link_msg(src_msg)
         )
         await src_msg.edit(
-            content = EMOTE_FROM + " <#" + str(dst_msg.channel.id) + "> " + gen_link_msg(dst_msg)
+            content=EMOTE_FROM +
+            f" <#{dst_msg.channel.id}> " + gen_link_msg(dst_msg)
         )
 
 
