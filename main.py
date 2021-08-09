ee4b9c718ab1cc2789f983d0130d8a020c87dd6d
Thomas Quig <trquig1@gmail.com>
2021-08-09 00:00:19 -0500

    Update main.py, testing github actions
    H

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ slash = SlashCommand(bot, sync_commands=True, sync_on_cog_reload=True)
 async def on_ready() -> None:
 
     logger.info(f"{bot.user} connected.")
+    logger.info(f"Hello, pwnybot is now live... do with that information what you will")
     await bot.change_presence(activity=discord.Game(name="/help"))
 
 
