9a78c380181dc4960c2bde15ec86733fd7ed07f4
Peter Stenger <peter.a.stenger@gmail.com>
2021-08-25 13:38:13 -0500

    start work on new CTF role system

--- cogs/binary/binary.py
+++ cogs/binary/binary.py
@@ -1,7 +1,7 @@
 from discord.ext.commands import Bot
 from discord.ext import commands
 from discord_slash import SlashContext
-from lib.util import subcommand_decorator
+from lib.util import subcommand_decorator, Timeout
 import subprocess
 import os
 import tempfile
@@ -15,27 +15,33 @@ class Binary(commands.Cog):
         self.bot = bot
 
     @subcommand_decorator()
-    async def rop(self, ctx: SlashContext) -> None:
+    async def rop(self, ctx: SlashContext):
         """Use ROPGadget to analyze file for gadgets
-
         """
         await ctx.defer()
-        messages = await ctx.channel.history(limit=100).flatten()
         try:
-            to_analyze = [m for m in messages if m.author.id ==
-                          ctx.author.id and len(m.attachments) >= 1][0].attachments[0]
-            await to_analyze.save('to_analyze')
-            cmd = ['ROPgadget', '--binary', 'to_analyze']
-            new_file, filename = tempfile.mkstemp()
-            subprocess.run(cmd, stdout=new_file)
+            messages = await ctx.channel.history(limit=100).flatten()
+            with Timeout(seconds=10):
+                to_analyze = [m for m in messages if m.author.id ==
+                              ctx.author.id and len(m.attachments) >= 1][0].attachments[0]
 
-            await ctx.send(file=discord.File(filename, 'gadgets.txt'))
-            # Cleanup
-            os.remove(filename)
-            os.remove(to_analyze)
+                _, temp_to_analyze_name = tempfile.mkstemp()
+                await to_analyze.save(temp_to_analyze_name)
+
+                new_file, filename = tempfile.mkstemp()
+                cmd = ['ROPgadget', '--binary', temp_to_analyze_name]
+                subprocess.run(cmd, stdout=new_file, check=True)
+                await ctx.send(file=discord.File(filename, 'gadgets.txt'))
+
+                os.remove(filename)
+                os.remove(temp_to_analyze_name)
         except IndexError:
             await ctx.send(":x: Attach the file before analyzing.")
             return
+        except TimeoutError:
+            await ctx.send(':x: Result timed out')
+        except subprocess.CalledProcessError:
+            await ctx.send(':x: ROPgadget threw an error')
 
 
 def setup(bot: Bot) -> None:
