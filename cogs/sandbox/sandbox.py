8b754453f439cb2fb1d5b8033261f3206d85f6d3
Peter Stenger <peter.a.stenger@gmail.com>
2021-08-25 22:36:14 -0500

    new actual sandbox

--- cogs/sandbox/sandbox.py
+++ cogs/sandbox/sandbox.py
@@ -2,57 +2,81 @@ from discord.ext.commands import Bot
 from discord.ext import commands
 from discord_slash import SlashContext
 from lib.util import command_decorator, subcommand_decorator
+from dataclasses import dataclass
+import epicbox
 
 
-'''
-compile_restricted uses a predefined policy that checks and modify the source code and checks
- against a restricted subset of the Python language. The compiled source code is still 
- executed against the full available set of library modules and methods.
-'''
-# Zope security imports
-from RestrictedPython import compile_restricted
-from RestrictedPython import Eval
-from RestrictedPython import Guards
-from RestrictedPython import safe_globals
-from RestrictedPython import utility_builtins
-from RestrictedPython.PrintCollector import PrintCollector
-import builtins
-from lib.util import Timeout
-from .zope_sandbox import protected_inplacevar
+@dataclass
+class Language:
+    file: str
+    language: str
+    image: str
+    command: str
+
+    def run(self, code):
+        limits = {
+            # CPU time in seconds, None for unlimited
+            'cputime': 60,
+            # Real time in seconds, None for unlimited
+            'realtime': 120,
+            # Memory in megabytes, None for unlimited
+            'memory': 64,
+
+            # limit the max processes the sandbox can have
+            # -1 or None for unlimited(default)
+            'processes': -1,
+        }
+        files = [{'name': self.file, 'content': code.encode('utf-8')}]
+        return epicbox.run(self.language, self.command,
+                           files=files, limits=limits)
 
 
-def execute_untrusted(code):
-    """Interprets the given python code inside a safe execution environment"""
+languages = [
+    Language('main.py', 'python',
+             'python:latest', 'python3 main.py'),
+    Language('main.c', 'c', 'gcc:latest',
+             'gcc main.c -o main && ./main'),
+    Language('Main.hs', 'haskell', 'haskell:latest',
+             'ghc Main.hs && ./Main'),
+    Language('main.bf', 'brainfuck',
+             'esolang/brainfuck-esotope:latest', 'brainfuck-esotope main.bf'),
+    Language('Main.java', 'java', 'openjdk:15',
+             'javac Main.java && java Main'),
+    Language('main.rs', 'rust', 'rust:latest',
+             'rustc main.rs && ./main'),
+    Language('main.cpp', 'cpp', 'gcc:latest',
+             'g++ main.cpp -o main && ./main'),
+    Language('main.cs', 'c#', 'mono:latest',
+             'mcs -out:main.exe main.cs && mono main.exe'),
+    Language('main.go', 'golang', 'golang:latest', 'go run main.go'),
+    Language('main.js', 'js', 'node:latest', 'node main.js'),
+    Language('main.ts', 'ts', 'zachdeibert/typescript:latest',
+             'tsc main.ts && node main.js'),
+    Language('main.dart', 'dart',
+             'google/dart:latest', 'dart main.dart'),
+    Language('main.swift', 'swift', 'swift:latest',
+             'swiftc -o main main.swift && ./main'),
+    Language('main.clj', 'clojure', 'clojure:latest', 'clj main.clj'),
+    Language('main.lua', 'lua',
+             'woahbase/alpine-lua:latest', 'lua main.lua'),
+    Language('main.ml', 'ocaml', 'ocaml/opam2:latest',
+             'ocamlc -o main main.ml && ./main'),
+    Language('main.php', 'php', 'php:latest',
+             'php main.php'),
+    Language('main.r', 'r', 'r-base:latest',
+             'Rscript main.r'),
+    Language('main.rb', 'ruby', 'ruby:latest', 'ruby main.rb'),
+    Language('main.fsx', 'fsharp',
+             'fsharp:latest', 'fsharpi main.fsx'),
+    Language('main.kt', 'kotlin', 'zenika/kotlin:latest',
+             'kotlinc main.kt -include-runtime -d main.jar && java -jar main.jar'),
+    Language('main.scala', 'scala',
+             'hseeberger/scala-sbt:8u222_1.3.5_2.13.1', 'scala Main.scala'),
+    Language('main.sh', 'bash', 'bash:latest', 'bash main.sh'),
+    Language('main.lsp', 'lisp', 'daewok/lisp-devel:latest',
+             'sbcl --script main.lsp'),
 
-    def no_import(name, *args, **kwargs):
-        raise ImportError('No imports for you!!!')
-
-    code += "\nresults = printed"
-    byte_code = compile_restricted(
-        code,
-        filename="<string>",
-        mode="exec",
-    )
-    policy_globals = {**safe_globals, **utility_builtins}
-    print(policy_globals)
-
-    del policy_globals['string']
-    del policy_globals['random']
-    del policy_globals['whrandom']
-    policy_globals['__builtins__']['__metaclass__'] = type
-    policy_globals['__builtins__']['__name__'] = type
-    policy_globals['__builtins__']['__import__'] = no_import
-    policy_globals['_getattr_'] = Guards.safer_getattr
-    policy_globals['_getiter_'] = Eval.default_guarded_getiter
-    policy_globals['_getitem_'] = Eval.default_guarded_getitem
-    policy_globals['_write_'] = Guards.full_write_guard
-    policy_globals['_print_'] = PrintCollector
-    policy_globals['_iter_unpack_sequence_'] = Guards.guarded_iter_unpack_sequence
-    policy_globals['_unpack_sequence_'] = Guards.guarded_unpack_sequence
-    policy_globals['enumerate'] = enumerate
-    policy_globals['_inplacevar_'] = protected_inplacevar
-    exec(byte_code, policy_globals, None)
-    return policy_globals["results"]
+]
 
 
 class Sandbox(commands.Cog):
@@ -60,11 +84,15 @@ class Sandbox(commands.Cog):
 
     def __init__(self, bot: Bot) -> None:
         self.bot = bot
+        epicbox.configure(
+            profiles=[
+                epicbox.Profile(l.language, l.image) for l in languages
+            ]
+        )
 
-    # this command is disabled until further notice
-    # @command_decorator()
-    async def exec(self, ctx: SlashContext) -> None:
-        """Execute your last code block message in a restricted python environment.
+    @command_decorator(lang={'description': 'Language to execute last block in', "choices": [l.language for l in languages]})
+    async def exec(self, ctx: SlashContext, lang: str) -> None:
+        """Execute your last code block message
 
         """
         await ctx.defer()
@@ -75,20 +103,23 @@ class Sandbox(commands.Cog):
         except IndexError:
             await ctx.send(":x: You haven't made any code blocks!")
             return
-        code = last_code_block[3:-3]
-        if code.startswith('python'):
-            code = code[len('python'):]
-        elif code.startswith('py'):
-            code = code[len('py'):]
-        try:
-            with Timeout(seconds=10):
-                exec_result = execute_untrusted(code)
-                await ctx.send(f':white_check_mark: Executed:```py\n{code}\n```\nResult:```{exec_result if exec_result else "None"}```')
+        code = '\n'.join(
+            list(filter(lambda x: '```' not in x, last_code_block.split('\n'))))
 
-        except TimeoutError:
-            await ctx.send(':x: Error: Timeout of 10s exceeded')
-        except SystemExit:
-            await ctx.send(':x: Error: Don\'t kill the bot please :(')
+        lang = next(filter(lambda x: x.language == lang, languages))
+
+        result = lang.run(code)
+
+        if result['timeout']:
+            output = ':x: TimeoutError: program runtime exceeded timeout.'
+        elif result['oom_killed']:
+            output = ':x: MemoryError: program exceeded memory limit'
+        else:
+            output = f'Exit code: {result["exit_code"]}\n'
+            output += f'Duration: {result["duration"]}\n'
+            output += f'```\n{result["stdout"].decode("utf-8")}```'
+
+        await ctx.send(output)
 
 
 def setup(bot: Bot) -> None:
