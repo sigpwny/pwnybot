7519bc809a4dc3037ea146bd3c94bbb0ae6e56ac
Peter Stenger <peter.a.stenger@gmail.com>
2021-09-08 22:28:50 -0500

    god i might be the smartest person of all time i was missing 1 line of code

--- cogs/ctf/ctf.py
+++ cogs/ctf/ctf.py
@@ -98,12 +98,11 @@ class CTF(commands.Cog):
         '''
         For UIUC members: Opts into CTFs automatically
         '''
-        print(ctx.guild.roles)
+        await ctx.defer()
+
         ctf_role = discord.utils.get(ctx.guild.roles, id=CTF_ROLE_ID)
         uiuc_role = discord.utils.get(ctx.guild.roles, id=UIUC_ROLE_ID)
-        if ctf_role is None or uiuc_role is None:
-            raise ValueError(
-                "CTF ROLE: {ctf_role} CTF ID: {CTF_ROLE_ID} UIUC ROLE: {uiuc_role} UIUC ID: {UIUC_ROLE_ID}")
+
         if uiuc_role in ctx.author.roles:
             await ctx.author.add_roles(ctf_role)
             await ctx.send(':white_check_mark: You were given the CTF role.')
@@ -116,11 +115,13 @@ class CTF(commands.Cog):
         '''
         For UIUC members: Opts out of CTFs automatically
         '''
+        await ctx.defer()
+
         ctf_role = discord.utils.get(ctx.guild.roles, id=CTF_ROLE_ID)
         uiuc_role = discord.utils.get(ctx.guild.roles, id=UIUC_ROLE_ID)
         if uiuc_role in ctx.author.roles:
             await ctx.author.remove_roles(ctf_role)
-            await ctx.send(':white_check_mark: You are no longer opted into CTFs.')
+            await ctx.send(':white_check_mark: You are no longer opted into CTFs :sob:.')
         else:
             await ctx.send(':x: You must be UIUC verified to use this command.')
 
