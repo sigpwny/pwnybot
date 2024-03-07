8aef207db7249576d928981d916a0b1700b47e47
jupiter <pengjupiter5@gmail.com>
2024-03-06 22:03:15 -0600

    add back template

--- cogs/template/template.py
+++ cogs/template/template.py
@@ -1,9 +1,22 @@
-import interactions
+from interactions import Extension, SlashContext
 
-import lib.util
+from lib.util import command_decorator, subcommand_decorator
 
 
-class Template(interactions.Extension):
-    @interactions.slash_command(scopes=lib.config.GUILD_IDS)
-    async def asd(self, ctx: interactions.SlashContext):
-        await ctx.send("aosidjw")
+class Template(Extension):
+    '''Describe the cog'''
+
+    @command_decorator(message={'description': "The message"}, times={'description': "# of times max 3"})
+    async def reverserepeat(self, ctx: SlashContext, message: str, times: int = 1) -> None:
+        """The reverserepeat command is pretty epic!! (/reverserepeat)
+
+        """
+        for _ in range(min(times, 3)):
+            await ctx.send(message[::-1])
+
+    @subcommand_decorator(message={'description': "The message"})
+    async def say(self, ctx: SlashContext, message: str) -> None:
+        """The message command is pretty epic!! (/template say)
+
+        """
+        await ctx.send(message)
