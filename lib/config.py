04f88268e91c570efc7a19da3853b9eefb8f1802
jupiter <pengjupiter5@gmail.com>
2024-03-08 19:23:35 -0600

    move challenge categories to config file

--- lib/config.py
+++ lib/config.py
@@ -4,5 +4,5 @@ import os
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD_IDS = os.getenv('GUILD_IDS', "").split(",")
 CTF_CATEGORY_CHANNELS = os.getenv("CTF_CATEGORY_CHANNELS", "").split(",")
-CHALLENGE_CATEGORIES = os.getenv("CHALLENGE_CATEGORIES", "").split(",")
+CHALLENGE_CATEGORIES = ["crypto", "forensics", "misc", "networking", "pwn", "osint", "rev", "web"]
 FORUM_GENERAL_CHANNEL = "General"
