680e5c8d1647b7ab332104a548845aabbb866ffd
jupiter <pengjupiter5@gmail.com>
2024-03-10 12:59:07 -0700

    remove networking addresses https://github.com/sigpwny/pwnybot/pull/30#discussion_r1518706955

--- lib/config.py
+++ lib/config.py
@@ -7,5 +7,5 @@ CTF_CATEGORY_CHANNELS = os.getenv("CTF_CATEGORY_CHANNELS", "").split(",")
 CTF_ROLES = os.getenv("CTF_ROLES", "").split(",")
 UIUC_ROLES = os.getenv("UIUC_ROLES", "").split(",")
 
-CHALLENGE_CATEGORIES = ["crypto", "forensics", "misc", "networking", "pwn", "osint", "rev", "web"]
+CHALLENGE_CATEGORIES = ["crypto", "forensics", "misc", "pwn", "osint", "rev", "web"]
 FORUM_GENERAL_CHANNEL = "General"
