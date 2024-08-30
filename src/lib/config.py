864523f9542554c215ad002a078a4e558a33f69f
Minh Duong <git@whitehoodhacker.net>
2024-08-30 16:18:20 -0500

    Add config file and private roles (#32)
    * Add config file support
    
    * Add roles cog
    
    * Move into `src` folder and update docker build
    
    * Add additional config options

---
+++ src/lib/config.py
@@ -0,0 +1,27 @@
+import os
+import yaml
+import logging
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+config = {}
+try:
+    with open("config.yml", "r") as f:
+        try:
+            config = yaml.safe_load(f)
+        except yaml.YAMLError as e:
+            logger.error(f"Error loading config: {e}")
+            exit(1)
+except FileNotFoundError:
+    logger.warning("No config file found.")
+
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN') or config.get("discord_token")
+GUILD_IDS = os.getenv('GUILD_IDS', "").split(",") or config.get("guild_ids", [])
+CTF_CATEGORY_CHANNELS = os.getenv("CTF_CATEGORY_CHANNELS", "").split(",") or config.get("ctf_category_channels", [])
+CTF_ROLES = os.getenv("CTF_ROLES", "").split(",") or config.get("ctf_roles", [])
+UIUC_ROLES = os.getenv("UIUC_ROLES", "").split(",") or config.get("uiuc_roles", [])
+PRIVATE_ROLES = config.get("private_roles", [])
+
+CHALLENGE_CATEGORIES = ["crypto", "forensics", "misc", "pwn", "osint", "rev", "web"]
+FORUM_GENERAL_CHANNEL = "General"
